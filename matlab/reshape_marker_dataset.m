ca7063ad2c99db81994505bfd653b91948eff577
tbirdal <tbirdal@stanford.edu>
2019-11-15 20:00:48 -0800

    before pull

--- matlab/reshape_marker_dataset.m
+++ matlab/reshape_marker_dataset.m
@@ -1,30 +1,58 @@
 
 folder = 'D:\Data\meas_sync\marker1';
-N = 10;
+N = 9;
 % 
-% I = [];
-% fid = fopen([folder '/QRel.txt'], 'w');
-% fidEdges = fopen([folder '/Edges.txt'], 'w');
-% for i=1:N
-%     for j=1:N
-%         if (i~=j)
-%             fn = sprintf('%s/out/quat_%02d_%02d.txt', folder, i, j);
-%             
-%             if (exist(fn,'file'))
-%                 Q = load(fn);
-%                 I = [I; int32([i,j])];
-%                 
-%                 fprintf(fid, '%g %g %g %g\n', Q);
-%                 fprintf(fid, '\n');
-%                 fprintf(fidEdges, '%d %d\n', i, j);
-%             end
-%             
-%         end
-%     end
-% end
+I = [];
+fid = fopen([folder '/QRel.txt'], 'w');
+fidEdges = fopen([folder '/Edges.txt'], 'w');
+for i=0:N
+    for j=0:N
+        if (i~=j)
+            fn = sprintf('%s/out/quat_%02d_%02d.txt', folder, i, j);
+            
+            if (exist(fn,'file'))
+                Q = load(fn)';
+                I = [I; int32([i,j])];
+                
+                fprintf(fid, '%g ', Q(:));
+                fprintf(fid, '\n');
+                fprintf(fidEdges, '%d %d\n', i+1, j+1);
+            end
+            
+        end
+    end
+end
+fclose(fid);
+fclose(fidEdges);
 
-fnAbs = 'C:\Users\tolga\Box Sync\gitforks\OptimalTransportSync\py\data\marker_Qabs.txt';
+fnAbs = 'D:\Data\meas_sync\marker1\marker_Qabs.txt';
+fnRel = 'D:\Data\meas_sync\marker1\marker_Qrel.txt';
+fnEdges = 'D:\Data\meas_sync\marker1\Edges.txt';
+
+
+fnEdges = 'C:\Users\tolga\Box Sync\gitforks\OptimalTransportSync\py\marker_Edges.txt';
+Edges = load (fnEdges);
+Edges = I+1;
+
+fnAbs = 'C:\Users\tolga\Box Sync\gitforks\OptimalTransportSync\py\marker_Qabs.txt';
+fnRel = 'C:\Users\tolga\Box Sync\gitforks\OptimalTransportSync\py\marker_Qrel.txt';
 Q = load (fnAbs);
+Qabs = Q;
+Qrel = load (fnRel);
+qrelInsert = t_qconj(Qabs(2,:));
+Qrel = [qrelInsert qrelInsert qrelInsert qrelInsert; Qrel];
+qrelInsert = (Qabs(2,:));
+Qrel = [qrelInsert qrelInsert qrelInsert qrelInsert; Qrel];
+
+% insert a camera
+Qabs = [1 0 0 0; Qabs];
+Edges = Edges + 1;
+Edges = [[1 2]; Edges];
+
+writematrix(Qabs, fnAbs, 'Delimiter', ' ');
+writematrix(Qrel, fnRel, 'Delimiter', ' ');
+writematrix(Edges, fnEdges, 'Delimiter', ' ');
+
 for i=1:size(Q,1)
     q = Q(i, :);
     for j=1:4:length(q)
@@ -36,8 +64,7 @@ for i=1:size(Q,1)
 end
 writematrix(Q, fnAbs, 'Delimiter', ' ');
 
-fclose(fid);
-fclose(fidEdges);
+
 %save([folder '/Edges.txt'], 'I', '-ascii');
 
 Q=[ 3.55603171e-30, -1.18534390e-30,  7.07106781e-01,  7.07106781e-01
